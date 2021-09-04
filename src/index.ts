de08e069f13dbdadb41cc99fe1813bb51419310d
Dominik Panzer <dominik.panzer1982@googlemail.com>
2021-09-04 23:01:45 +0200

    add fraction diff  denominator without simplifying green

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ export class Fraction {
   constructor(readonly numerator: number, readonly denominator: number = 1) {}
 
   public add(that: Fraction): Fraction {
-    return new Fraction(this.numerator + that.numerator, this.denominator);
+    return new Fraction(
+      this.numerator * that.denominator + that.numerator * this.denominator,
+      this.denominator * that.denominator
+    );
   }
 }
