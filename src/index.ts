6e3da4ccd8fb539f0b0a512270ab47ee65456677
Dominik Panzer <dominik.panzer1982@googlemail.com>
2021-09-04 22:58:19 +0200

    add fraction same denominator without simplifying green

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
 export class Fraction {
-  constructor(readonly numerator: number) {}
+  constructor(readonly numerator: number, readonly denominator: number = 1) {}
 
   public add(that: Fraction): Fraction {
-    return new Fraction(this.numerator + that.numerator);
+    return new Fraction(this.numerator + that.numerator, this.denominator);
   }
 }
